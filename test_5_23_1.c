69ab1067ce9941a9661781e1582aef8a87ef56bb
FrankHuCode <105832441+FrankHuCode@users.noreply.github.com>
2022-05-24 14:51:22 +0800

    Add files via upload

---
+++ test_5_23_1.c
@@ -0,0 +1,81 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+class Solution {
+public:
+    int divide(int a, int b) {
+        // 考虑被除数为最小值的情况
+        if (a == INT_MIN) {
+            if (b == 1) {
+                return INT_MIN;
+            }
+            if (b == -1) {
+                return INT_MAX;
+            }
+        }
+        // 考虑除数为最小值的情况
+        if (b == INT_MIN) {
+            return a == INT_MIN ? 1 : 0;
+        }
+        // 考虑被除数为 0 的情况
+        if (a == 0) {
+            return 0;
+        }
+
+        // 一般情况，使用二分查找
+        // 将所有的正数取相反数，这样就只需要考虑一种情况
+        bool rev = false;
+        if (a > 0) {
+            a = -a;
+            rev = !rev;
+        }
+        if (b > 0) {
+            b = -b;
+            rev = !rev;
+        }
+
+        // 快速乘
+        auto quickAdd = [](int y, int z, int x) {
+            // x 和 y 是负数，z 是正数
+            // 需要判断 z * y >= x 是否成立
+            int result = 0, add = y;
+            while (z) {
+                if (z & 1) {
+                    // 需要保证 result + add >= x
+                    if (result < x - add) {
+                        return false;
+                    }
+                    result += add;
+                }
+                if (z != 1) {
+                    // 需要保证 add + add >= x
+                    if (add < x - add) {
+                        return false;
+                    }
+                    add += add;
+                }
+                // 不能使用除法
+                z >>= 1;
+            }
+            return true;
+        }
+
+        int left = 1, right = INT_MAX, ans = 0;
+        while (left <= right) {
+            // 注意溢出，并且不能使用除法
+            int mid = left + ((right - left) >> 1);
+            bool check = quickAdd(b, mid, a);
+            if (check) {
+                ans = mid;
+                // 注意溢出
+                if (mid == INT_MAX) {
+                    break;
+                }
+                left = mid + 1;
+            }
+            else {
+                right = mid - 1;
+            }
+        }
+
+        return rev ? -ans : ans;
+    }
+}
