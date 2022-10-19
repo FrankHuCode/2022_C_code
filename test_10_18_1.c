f0526ca1adb9731a7c60fd4f5d804321f5e26b38
FrankHuCode <105832441+FrankHuCode@users.noreply.github.com>
2022-10-19 19:09:30 +0800

    Add files via upload

---
+++ test_10_18_1.c
@@ -0,0 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+//文件
+
+//程序文件（源文件.c、目标文件.obj、可执行文件.exe）、数据文件
+
+//文件操作
+//fopen fclose fread fwrite
+// fgetc fgets fputc fputs
+//  
+//scanf：针对标准输入的格式化的输入语句-stdin -键盘 
+//fscanf：针对所有输入流的格式化的输入语句-stdin/文件流
+//sscanf：从一个字符串中读取一个格式化的数据
+// 
+// 
+//printf：针对标准输出的格式化的输出语句-stdout -屏幕 
+//fprintf ：针对所有输出流的格式化的输出语句--stdout/文件流
+//sprintf：把一个格式化的数据，转换成字符串
+
+
+//文件缓冲区
